dc882f93eb8c6448953a64b815a077ffff0b7b5c
SMHRD <SMHRD@220.93.229.178>
2024-01-26 14:38:07 +0900

    Main 기초

--- pokemonname/src/Main/PokemonProgram.java
+++ pokemonname/src/Main/PokemonProgram.java
@@ -1,9 +1,83 @@
 package Main;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import Model.MemberDAO;
+import Model.MemberDTO;
+import Model.PokemonDAO;
+import Model.PokemonDTO;
+
 public class PokemonProgram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			System.out.print("포켓몬 너의 이름은");
+			System.out.print("[1]회원가입 [2]로그인 [3]종료 >>");
+			int choice = sc.nextInt();
+			
+			if(choice==1) {
+				System.out.print("가입할 아이디 입력 : ");
+				String joinId = sc.next();
+				
+				int result = pdao.idCheck(joinId);
+				if(result==0) {
+					System.out.println("사용가능한 id입니다.");
+				}else continue;
+				
+				System.out.print("가입할 비밀번호 입력 : ");
+				String joinPw = sc.next();
+				
+				PokemonDTO dto = new PokemonDTO();
+				dto.setID(joinId);
+				dto.setPW(joinPw);
+				
+			}else if(choice==2) {
+				System.out.print("아이디 입력 : ");
+				String id = sc.next();
+				System.out.print("비밀번호 입력 : ");
+				String pw = sc.next();
+				
+				PokemonDTO dto = new PokemonDTO();
+				dto.setID(id);
+				dto.setPW(pw);
+				
+				PokemonDTO result = pdao.login(dto);
+				
+				//=======플레이
+				if(result != null) {
+					System.out.println("===메뉴 선택===");
+					System.out.println("[1]플레이 [2]랭킹보기 [3]나의 기록 확인 [4]로그아웃 >>");
+					int menu = sc.nextInt();
+					if(menu==1) {
+						
+						
+					}else if(menu==2) {
+						ArrayList<PokemonDTO> list=pdao.rank(null);
+						System.out.println("순위\t아이디\t점수");
+						for(int i = 0;i<list.size();i++) {
+							
+							PokemonDTO dto1 = list.get(i);
+					    	System.out.println((i+1)+"\t" +dto1.getId() +"\t" +  dto1.getScore());
+					    }
+					
+					}else if(menu==3) {
+						ArrayList<PokemonDTO> list = pdao.history(result.getID());
+						for(int i=0;i<list.size();i++) {
+							System.out.println(list.get(i).getscore());
+						}
+						
+					}else break;
+						
+					
+				}
+				
+				
+				
+			}else break;
+		}
 
 	}
 
