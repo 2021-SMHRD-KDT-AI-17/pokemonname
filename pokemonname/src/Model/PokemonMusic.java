3a91178b4265f69f2d8a555208eab9fc8e2b83e7
SMHRD <SMHRD@220.93.229.178>
2024-01-28 13:55:36 +0900

    bgm 틀

--- pokemonname/src/Model/PokemonMusic.java
+++ pokemonname/src/Model/PokemonMusic.java
@@ -1,5 +1,20 @@
 package Model;
 
 public class PokemonMusic {
+	
+	private String path;
+	private String  title;
+	public PokemonMusic(String path, String title) {
+		super();
+		this.path = path;
+		this.title = title;
+	}
+	public String getPath() {
+		return path;
+	}
+	public String getTitle() {
+		return title;
+	}
+	
 
 }
