070b5c28a2aef417f6422f7663c49dc79c989a73
smhrd <smhrd@121.147.52.249>
2024-01-29 09:14:55 +0900

    메인 / 틀렸을때 공격당하는 모습 보이고 다음문제 넘어가게 지연 메서드 추가 / 명택

--- pokemonname/PokemonDB.sql
+++ pokemonname/PokemonDB.sql
@@ -11,4 +11,4 @@ select * from score where rownum <= 5 and id like 'bome' order by indate desc;
 select * from score where id like 'bome' order by indate desc;
 
 
-select * from score where rownum <= 5 and id like 'bome' order by indate;
+select * from score where rownum <= 5 and id like 'mt' order by indate desc;
